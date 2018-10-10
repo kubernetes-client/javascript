a8e88615928a0c9ff17684657c59db25bee5a3e1
Brendan Burns <bburns@microsoft.com>
2018-10-09 20:34:23 -0700

    Fix a bug in attach where stdin wasn't copied. Add tests.

--- node-client/src/attach.ts
+++ node-client/src/attach.ts
@@ -2,18 +2,22 @@ import querystring = require('querystring');
 import stream = require('stream');
 
 import { KubeConfig } from './config';
-import { WebSocketHandler } from './web-socket-handler';
+import { WebSocketHandler, WebSocketInterface } from './web-socket-handler';
 
 export class Attach {
-    public 'handler': WebSocketHandler;
+    public 'handler': WebSocketInterface;
 
-    public constructor(config: KubeConfig) {
-        this.handler = new WebSocketHandler(config);
+    public constructor(config: KubeConfig, websocketInterface?: WebSocketInterface) {
+        if (websocketInterface) {
+            this.handler = websocketInterface;
+        } else {
+            this.handler = new WebSocketHandler(config);
+        }
     }
 
-    public attach(namespace: string, podName: string, containerName: string,
-                  stdout: stream.Writable | any, stderr: stream.Writable | any, stdin: stream.Readable | any,
-                  tty: boolean): Promise<void> {
+    public async attach(namespace: string, podName: string, containerName: string,
+                        stdout: stream.Writable | any, stderr: stream.Writable | any, stdin: stream.Readable | any,
+                        tty: boolean): Promise<void> {
         const query = {
             container: containerName,
             stderr: stderr != null,
@@ -23,13 +27,12 @@ export class Attach {
         };
         const queryStr = querystring.stringify(query);
         const path = `/api/v1/namespaces/${namespace}/pods/${podName}/attach?${queryStr}`;
-        const promise = this.handler.connect(path, null, (streamNum: number, buff: Buffer): boolean => {
+        const conn = await this.handler.connect(path, null, (streamNum: number, buff: Buffer): boolean => {
             WebSocketHandler.handleStandardStreams(streamNum, buff, stdout, stderr);
             return true;
         });
-        const result = new Promise<void>((resolvePromise, reject) => {
-            promise.then(() => resolvePromise(), (err) => reject(err));
-        });
-        return result;
+        if (stdin != null) {
+            WebSocketHandler.handleStandardInput(conn, stdin);
+        }
     }
 }
