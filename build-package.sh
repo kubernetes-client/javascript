a17c30f799afb06ea7a74acfdfd23608ee508500
Brendan Burns <bburns@microsoft.com>
2019-05-31 15:12:01 -0700

    Update the package build script.

--- build-package.sh
+++ build-package.sh
@@ -3,7 +3,17 @@
 # exit on any error
 set -e
 
+# validate branches
 . ./pre-check.sh
 
-tsc
+# pre-requisites
+npm run lint
+npm test
+
+# clean any old files
+npm run clean
+npm install
+
+# build
+npm run build
 npm pack
