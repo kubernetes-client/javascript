1d89fcd85ff4da05aaf9ed658ef767baf48cab3d
Silas Boyd-Wickizer <silas@godaddy.com>
2019-08-19 13:51:44 -0700

    Force GEN_COMMIT

--- generate-client.sh
+++ generate-client.sh
@@ -7,15 +7,14 @@ set -o pipefail
 . settings
 
 if [[ -z ${GEN_ROOT:-} ]]; then
+    : "${GEN_COMMIT?Need to set GEN_COMMIT to kubernetes-client/gen commit}"
     TEMP_FOLDER=$(mktemp -d)
     trap "rm -rf ${TEMP_FOLDER}" EXIT SIGINT
 
     GEN_ROOT="${TEMP_FOLDER}/gen"
     echo ">>> Cloning gen repo"
     git clone --recursive https://github.com/kubernetes-client/gen.git "${GEN_ROOT}"
-    if [[ ! -z ${GEN_COMMIT:-} ]]; then
-        (cd ${GEN_ROOT} && git checkout ${GEN_COMMIT})
-    fi
+    (cd ${GEN_ROOT} && git checkout ${GEN_COMMIT})
 else
     echo ">>> Reusing gen repo at ${GEN_ROOT}"
 fi
