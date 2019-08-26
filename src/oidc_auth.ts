fb1ee0c9069bf686cdc480d522c3a556fa61ec15
Louis Murray <louis.murray@ibm.com>
2019-08-25 21:40:34 -0400

    openid token management implementation
    openid token management impl
    
    oidc token management
    
    implement oidc token refreshing
    
    add openid-client
    
    add oidc refresh logic
    
    add tests for oidc logic
    
    introspect needs special urls so removing
    
    updated tests to reflect introspect not being able to be used
    
    prettier changes
    
    chore: use a version of open-id client that supports node 8
    
    fix id-token ref to id_token
    
    update token expiration to be more flexible
    
    replace the tokenExp with tokenExpirationCache
    
    update if for first time interaction with a user
    
    replace token expiration map with single expiration
    
    update test descriptions to be more accurate
    
    correct test to set initial expiration
    
    add oidc types
    
    reference types in class
    
    cast any so we can keep properties private
    
    prettier
    
    use expires_at over expiration

--- src/oidc_auth.ts
+++ src/oidc_auth.ts
@@ -1,10 +1,13 @@
 import https = require('https');
+import { Client, Issuer } from 'openid-client';
 import request = require('request');
 
 import { Authenticator } from './auth';
 import { User } from './config_types';
 
 export class OpenIDConnectAuth implements Authenticator {
+    // public for testing purposes.
+    private currentTokenExpiration = 0;
     public isAuthProvider(user: User): boolean {
         if (!user.authProvider) {
             return false;
@@ -12,19 +15,52 @@ export class OpenIDConnectAuth implements Authenticator {
         return user.authProvider.name === 'oidc';
     }
 
-    public async applyAuthentication(user: User, opts: request.Options | https.RequestOptions) {
-        const token = this.getToken(user);
+    /**
+     * Setup the authentication header for oidc authed clients
+     * @param user user info
+     * @param opts request options
+     * @param overrideClient for testing, a preconfigured oidc client
+     */
+    public async applyAuthentication(
+        user: User,
+        opts: request.Options | https.RequestOptions,
+        overrideClient?: any,
+    ) {
+        const token = await this.getToken(user, overrideClient);
         if (token) {
             opts.headers!.Authorization = `Bearer ${token}`;
         }
     }
 
-    private getToken(user: User): string | null {
-        if (!user.authProvider.config || !user.authProvider.config['id-token']) {
+    private async getToken(user: User, overrideClient?: Client): Promise<string | null> {
+        if (
+            !user.authProvider.config ||
+            !user.authProvider.config['id-token'] ||
+            !user.authProvider.config['client-id'] ||
+            !user.authProvider.config['client-secret'] ||
+            !user.authProvider.config['refresh-token'] ||
+            !user.authProvider.config['idp-issuer-url']
+        ) {
             return null;
         }
-        // TODO: Handle expiration and refresh here...
-        // TODO: Extract the 'Bearer ' to config.ts?
+        const client = overrideClient ? overrideClient : await this.getClient(user);
+        return this.refresh(user, client);
+    }
+
+    private async refresh(user: User, client: Client): Promise<string> {
+        if (Date.now() / 1000 > this.currentTokenExpiration) {
+            const newToken = await client.refresh(user.authProvider.config['refresh-token']);
+            user.authProvider.config['id-token'] = newToken.id_token;
+            this.currentTokenExpiration = newToken.expires_at || 0;
+        }
         return user.authProvider.config['id-token'];
+    }
+
+    private async getClient(user: User) {
+        const oidcIssuer = await Issuer.discover(user.authProvider.config['idp-issuer-url']);
+        return new oidcIssuer.Client({
+            client_id: user.authProvider.config['client-id'],
+            client_secret: user.authProvider.config['client-secret'],
+        });
     }
 }
--- src/oidc_auth_test.ts
+++ src/oidc_auth_test.ts
@@ -32,13 +32,15 @@ describe('OIDCAuth', () => {
         expect(auth.isAuthProvider(user)).to.equal(false);
     });
 
-    it('get a token if present', async () => {
-        const token = 'some token';
+    it('authorization should be undefined if token missing', async () => {
         const user = {
             authProvider: {
                 name: 'oidc',
                 config: {
-                    'id-token': token,
+                    'client-id': 'id',
+                    'client-secret': 'clientsecret',
+                    'refresh-token': 'refreshtoken',
+                    'idp-issuer-url': 'https://www.google.com/',
                 },
             },
         } as User;
@@ -46,14 +48,19 @@ describe('OIDCAuth', () => {
         const opts = {} as request.Options;
         opts.headers = [];
         await auth.applyAuthentication(user, opts);
-        expect(opts.headers.Authorization).to.equal(`Bearer ${token}`);
+        expect(opts.headers.Authorization).to.be.undefined;
     });
 
-    it('get null if token missing', async () => {
+    it('authorization should be undefined if client-id missing', async () => {
         const user = {
             authProvider: {
                 name: 'oidc',
-                config: {},
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-secret': 'clientsecret',
+                    'refresh-token': 'refreshtoken',
+                    'idp-issuer-url': 'https://www.google.com/',
+                },
             },
         } as User;
 
@@ -61,5 +68,143 @@ describe('OIDCAuth', () => {
         opts.headers = [];
         await auth.applyAuthentication(user, opts);
         expect(opts.headers.Authorization).to.be.undefined;
+    });
+
+    it('authorization should be undefined if client-secret missing', async () => {
+        const user = {
+            authProvider: {
+                name: 'oidc',
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-id': 'id',
+                    'refresh-token': 'refreshtoken',
+                    'idp-issuer-url': 'https://www.google.com/',
+                },
+            },
+        } as User;
+
+        const opts = {} as request.Options;
+        opts.headers = [];
+        await auth.applyAuthentication(user, opts);
+        expect(opts.headers.Authorization).to.be.undefined;
+    });
+
+    it('authorization should be undefined if refresh-token missing', async () => {
+        const user = {
+            authProvider: {
+                name: 'oidc',
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-id': 'id',
+                    'client-secret': 'clientsecret',
+                    'idp-issuer-url': 'https://www.google.com/',
+                },
+            },
+        } as User;
+
+        const opts = {} as request.Options;
+        opts.headers = [];
+        await auth.applyAuthentication(user, opts);
+        expect(opts.headers.Authorization).to.be.undefined;
+    });
+
+    it('authorization should be undefined if idp-issuer-url missing', async () => {
+        const user = {
+            authProvider: {
+                name: 'oidc',
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-id': 'id',
+                    'client-secret': 'clientsecret',
+                    'refresh-token': 'refreshtoken',
+                },
+            },
+        } as User;
+
+        const opts = {} as request.Options;
+        opts.headers = [];
+        await auth.applyAuthentication(user, opts, {});
+        expect(opts.headers.Authorization).to.be.undefined;
+    });
+
+    it('return token when it is still active', async () => {
+        const user = {
+            authProvider: {
+                name: 'oidc',
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-id': 'id',
+                    'client-secret': 'clientsecret',
+                    'refresh-token': 'refreshtoken',
+                    'idp-issuer-url': 'https://www.google.com/',
+                },
+            },
+        } as User;
+
+        const opts = {} as request.Options;
+        opts.headers = [];
+        (auth as any).currentTokenExpiration = Date.now() / 1000 + 1000;
+        await auth.applyAuthentication(user, opts, {});
+        expect(opts.headers.Authorization).to.equal('Bearer fakeToken');
+    });
+
+    it('return new token when the current expired', async () => {
+        const user = {
+            authProvider: {
+                name: 'oidc',
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-id': 'id',
+                    'client-secret': 'clientsecret',
+                    'refresh-token': 'refreshtoken',
+                    'idp-issuer-url': 'https://www.google.com/',
+                },
+            },
+        } as User;
+
+        const opts = {} as request.Options;
+        opts.headers = [];
+        (auth as any).currentTokenExpiration = Date.now() / 1000 - 5000;
+        const newExpiration = Date.now() / 1000 + 120;
+        await auth.applyAuthentication(user, opts, {
+            refresh: async (token) => {
+                return {
+                    expires_at: newExpiration,
+                    id_token: 'newToken',
+                };
+            },
+        });
+        expect(opts.headers.Authorization).to.equal('Bearer newToken');
+        expect((auth as any).currentTokenExpiration).to.equal(newExpiration);
+    });
+
+    it('return a new token when the its the first time we see this user', async () => {
+        const user = {
+            authProvider: {
+                name: 'oidc',
+                config: {
+                    'id-token': 'fakeToken',
+                    'client-id': 'id',
+                    'client-secret': 'clientsecret',
+                    'refresh-token': 'refreshtoken',
+                    'idp-issuer-url': 'https://www.google.com/',
+                },
+            },
+        } as User;
+
+        const opts = {} as request.Options;
+        opts.headers = [];
+        const newExpiration = Date.now() / 1000 + 120;
+        (auth as any).currentTokenExpiration = 0;
+        await auth.applyAuthentication(user, opts, {
+            refresh: async (token) => {
+                return {
+                    expires_at: newExpiration,
+                    id_token: 'newToken',
+                };
+            },
+        });
+        expect(opts.headers.Authorization).to.equal('Bearer newToken');
+        expect((auth as any).currentTokenExpiration).to.equal(newExpiration);
     });
 });
