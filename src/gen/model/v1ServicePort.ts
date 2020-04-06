05c1bdbe76ce946fd13b80208829f6803850067b
Jung Hong Kim <jungkim@dayta.ai>
2020-04-06 20:51:56 +0800

    fix: wrong targetPort type preventing service api calls

--- src/gen/model/v1ServicePort.ts
+++ src/gen/model/v1ServicePort.ts
@@ -35,7 +35,7 @@ export class V1ServicePort {
     /**
     * Number or name of the port to access on the pods targeted by the service. Number must be in the range 1 to 65535. Name must be an IANA_SVC_NAME. If this is a string, it will be looked up as a named port in the target Pod\'s container ports. If this is not specified, the value of the \'port\' field is used (an identity map). This field is ignored for services with clusterIP=None, and should be omitted or set equal to the \'port\' field. More info: https://kubernetes.io/docs/concepts/services-networking/service/#defining-a-service
     */
-    'targetPort'?: object;
+    'targetPort'?: string | number;
 
     static discriminator: string | undefined = undefined;
 
