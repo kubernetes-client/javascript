ba38464ef9413ae8073d70c3ae772652752653f6
Ray Foss <ray@vblip.com>
2020-04-24 20:35:19 -0500

    Shortened PR
    "enriched" to fit small size

--- examples/ingress.js
+++ examples/ingress.js
@@ -2,52 +2,26 @@ const k8s = require('@kubernetes/client-node')
 const kc = new k8s.KubeConfig()
 kc.loadFromDefault()
 
-// Using apiVersion networking.k8s.io/v1beta1, for versions before 1.14 use extensions/v1beta1
-const k8sApi = kc.makeApiClient(k8s.NetworkingV1beta1Api)
+const k8sApi = kc.makeApiClient(k8s.NetworkingV1beta1Api) // before 1.14 use extensions/v1beta1
+const clientIdentifier = 'my-subdomain'
 
-function createIngress(clientIdentifier) {
-  return {
-    apiVersions: 'networking.k8s.io/v1beta1',
-    kind: 'Ingress',
-    metadata: {
-      name: `production-custom-${clientIdentifier}`,
-      namespace: 'default'
-    },
-    spec: {
-      rules: [
-        {
-          name: `production-custom-${clientIdentifier}`,
-          host: `${clientIdentifier}.example.com`,
-          http: {
-            paths: [
-              {
-                backend: {
-                  serviceName: 'production-auto-deploy',
-                  servicePort: 5000
-                },
-                path: '/'
-              }
-            ]
-          }
-        }
-      ],
-      tls: [
-        {
-          hosts: [`${clientIdentifier}.example.com`]
-        }
-      ]
-    },
+k8sApi.createNamespacedIngress('default', {
+  apiVersions: 'networking.k8s.io/v1beta1',
+  kind: 'Ingress',
+  metadata: { name: `production-custom-${clientIdentifier}` },
+  spec: {
+    rules: [{
+      host: `${clientIdentifier}.example.com`,
+      http: {
+        paths: [{
+          backend: {
+            serviceName: 'production-auto-deploy',
+            servicePort: 5000
+          },
+          path: '/'
+        }]
+      }
+    }],
+    tls: [{ hosts: [`${clientIdentifier}.example.com`] }]
   }
-}
-
-function main () {
-  const clientIdentifier = 'poop'
-  const ingress = createIngress(clientIdentifier)
-  k8sApi.createNamespacedIngress('default', ingress)
-    .then(() => console.log('Success'))
-    .catch(e => {
-      console.log(e)
-    })
-}
-
-main()
+}).catch(e => console.log(e))
