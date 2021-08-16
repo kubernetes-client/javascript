a474ae07ff34f24c7ad800a37846db14cbd16d6b
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2021-08-16 17:43:50 -0400

    Change 'port' in v1HTTPGetAction to reflect proper type options

--- src/gen/model/v1HTTPGetAction.ts
+++ src/gen/model/v1HTTPGetAction.ts
@@ -32,7 +32,7 @@ export class V1HTTPGetAction {
     /**
     * Name or number of the port to access on the container. Number must be in the range 1 to 65535. Name must be an IANA_SVC_NAME.
     */
-    'port': object;
+    'port': string | number;
     /**
     * Scheme to use for connecting to the host. Defaults to HTTP.
     */
