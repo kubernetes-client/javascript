51c844d2a438318de01dc8a5a38d2980d5e542dd
Brendan Burns <bburns@microsoft.com>
2021-10-01 16:07:44 -0700

    Add generated code.

--- src/gen/api/apis.ts
+++ src/gen/api/apis.ts
@@ -2,20 +2,14 @@ export * from './admissionregistrationApi';
 import { AdmissionregistrationApi } from './admissionregistrationApi';
 export * from './admissionregistrationV1Api';
 import { AdmissionregistrationV1Api } from './admissionregistrationV1Api';
-export * from './admissionregistrationV1beta1Api';
-import { AdmissionregistrationV1beta1Api } from './admissionregistrationV1beta1Api';
 export * from './apiextensionsApi';
 import { ApiextensionsApi } from './apiextensionsApi';
 export * from './apiextensionsV1Api';
 import { ApiextensionsV1Api } from './apiextensionsV1Api';
-export * from './apiextensionsV1beta1Api';
-import { ApiextensionsV1beta1Api } from './apiextensionsV1beta1Api';
 export * from './apiregistrationApi';
 import { ApiregistrationApi } from './apiregistrationApi';
 export * from './apiregistrationV1Api';
 import { ApiregistrationV1Api } from './apiregistrationV1Api';
-export * from './apiregistrationV1beta1Api';
-import { ApiregistrationV1beta1Api } from './apiregistrationV1beta1Api';
 export * from './apisApi';
 import { ApisApi } from './apisApi';
 export * from './appsApi';
@@ -26,14 +20,10 @@ export * from './authenticationApi';
 import { AuthenticationApi } from './authenticationApi';
 export * from './authenticationV1Api';
 import { AuthenticationV1Api } from './authenticationV1Api';
-export * from './authenticationV1beta1Api';
-import { AuthenticationV1beta1Api } from './authenticationV1beta1Api';
 export * from './authorizationApi';
 import { AuthorizationApi } from './authorizationApi';
 export * from './authorizationV1Api';
 import { AuthorizationV1Api } from './authorizationV1Api';
-export * from './authorizationV1beta1Api';
-import { AuthorizationV1beta1Api } from './authorizationV1beta1Api';
 export * from './autoscalingApi';
 import { AutoscalingApi } from './autoscalingApi';
 export * from './autoscalingV1Api';
@@ -52,14 +42,10 @@ export * from './certificatesApi';
 import { CertificatesApi } from './certificatesApi';
 export * from './certificatesV1Api';
 import { CertificatesV1Api } from './certificatesV1Api';
-export * from './certificatesV1beta1Api';
-import { CertificatesV1beta1Api } from './certificatesV1beta1Api';
 export * from './coordinationApi';
 import { CoordinationApi } from './coordinationApi';
 export * from './coordinationV1Api';
 import { CoordinationV1Api } from './coordinationV1Api';
-export * from './coordinationV1beta1Api';
-import { CoordinationV1beta1Api } from './coordinationV1beta1Api';
 export * from './coreApi';
 import { CoreApi } from './coreApi';
 export * from './coreV1Api';
@@ -78,10 +64,6 @@ export * from './eventsV1Api';
 import { EventsV1Api } from './eventsV1Api';
 export * from './eventsV1beta1Api';
 import { EventsV1beta1Api } from './eventsV1beta1Api';
-export * from './extensionsApi';
-import { ExtensionsApi } from './extensionsApi';
-export * from './extensionsV1beta1Api';
-import { ExtensionsV1beta1Api } from './extensionsV1beta1Api';
 export * from './flowcontrolApiserverApi';
 import { FlowcontrolApiserverApi } from './flowcontrolApiserverApi';
 export * from './flowcontrolApiserverV1beta1Api';
@@ -96,8 +78,6 @@ export * from './networkingApi';
 import { NetworkingApi } from './networkingApi';
 export * from './networkingV1Api';
 import { NetworkingV1Api } from './networkingV1Api';
-export * from './networkingV1beta1Api';
-import { NetworkingV1beta1Api } from './networkingV1beta1Api';
 export * from './nodeApi';
 import { NodeApi } from './nodeApi';
 export * from './nodeV1Api';
@@ -120,16 +100,12 @@ export * from './rbacAuthorizationV1Api';
 import { RbacAuthorizationV1Api } from './rbacAuthorizationV1Api';
 export * from './rbacAuthorizationV1alpha1Api';
 import { RbacAuthorizationV1alpha1Api } from './rbacAuthorizationV1alpha1Api';
-export * from './rbacAuthorizationV1beta1Api';
-import { RbacAuthorizationV1beta1Api } from './rbacAuthorizationV1beta1Api';
 export * from './schedulingApi';
 import { SchedulingApi } from './schedulingApi';
 export * from './schedulingV1Api';
 import { SchedulingV1Api } from './schedulingV1Api';
 export * from './schedulingV1alpha1Api';
 import { SchedulingV1alpha1Api } from './schedulingV1alpha1Api';
-export * from './schedulingV1beta1Api';
-import { SchedulingV1beta1Api } from './schedulingV1beta1Api';
 export * from './storageApi';
 import { StorageApi } from './storageApi';
 export * from './storageV1Api';
@@ -142,7 +118,6 @@ export * from './versionApi';
 import { VersionApi } from './versionApi';
 export * from './wellKnownApi';
 import { WellKnownApi } from './wellKnownApi';
-import * as fs from 'fs';
 import * as http from 'http';
 
 export class HttpError extends Error {
@@ -152,14 +127,6 @@ export class HttpError extends Error {
     }
 }
 
-export interface RequestDetailedFile {
-    value: Buffer;
-    options?: {
-        filename?: string;
-        contentType?: string;
-    }
-}
+export { RequestFile } from '../model/models';
 
-export type RequestFile = string | Buffer | fs.ReadStream | RequestDetailedFile;
-
-export const APIS = [AdmissionregistrationApi, AdmissionregistrationV1Api, AdmissionregistrationV1beta1Api, ApiextensionsApi, ApiextensionsV1Api, ApiextensionsV1beta1Api, ApiregistrationApi, ApiregistrationV1Api, ApiregistrationV1beta1Api, ApisApi, AppsApi, AppsV1Api, AuthenticationApi, AuthenticationV1Api, AuthenticationV1beta1Api, AuthorizationApi, AuthorizationV1Api, AuthorizationV1beta1Api, AutoscalingApi, AutoscalingV1Api, AutoscalingV2beta1Api, AutoscalingV2beta2Api, BatchApi, BatchV1Api, BatchV1beta1Api, CertificatesApi, CertificatesV1Api, CertificatesV1beta1Api, CoordinationApi, CoordinationV1Api, CoordinationV1beta1Api, CoreApi, CoreV1Api, CustomObjectsApi, DiscoveryApi, DiscoveryV1Api, DiscoveryV1beta1Api, EventsApi, EventsV1Api, EventsV1beta1Api, ExtensionsApi, ExtensionsV1beta1Api, FlowcontrolApiserverApi, FlowcontrolApiserverV1beta1Api, InternalApiserverApi, InternalApiserverV1alpha1Api, LogsApi, NetworkingApi, NetworkingV1Api, NetworkingV1beta1Api, NodeApi, NodeV1Api, NodeV1alpha1Api, NodeV1beta1Api, OpenidApi, PolicyApi, PolicyV1Api, PolicyV1beta1Api, RbacAuthorizationApi, RbacAuthorizationV1Api, RbacAuthorizationV1alpha1Api, RbacAuthorizationV1beta1Api, SchedulingApi, SchedulingV1Api, SchedulingV1alpha1Api, SchedulingV1beta1Api, StorageApi, StorageV1Api, StorageV1alpha1Api, StorageV1beta1Api, VersionApi, WellKnownApi];
+export const APIS = [AdmissionregistrationApi, AdmissionregistrationV1Api, ApiextensionsApi, ApiextensionsV1Api, ApiregistrationApi, ApiregistrationV1Api, ApisApi, AppsApi, AppsV1Api, AuthenticationApi, AuthenticationV1Api, AuthorizationApi, AuthorizationV1Api, AutoscalingApi, AutoscalingV1Api, AutoscalingV2beta1Api, AutoscalingV2beta2Api, BatchApi, BatchV1Api, BatchV1beta1Api, CertificatesApi, CertificatesV1Api, CoordinationApi, CoordinationV1Api, CoreApi, CoreV1Api, CustomObjectsApi, DiscoveryApi, DiscoveryV1Api, DiscoveryV1beta1Api, EventsApi, EventsV1Api, EventsV1beta1Api, FlowcontrolApiserverApi, FlowcontrolApiserverV1beta1Api, InternalApiserverApi, InternalApiserverV1alpha1Api, LogsApi, NetworkingApi, NetworkingV1Api, NodeApi, NodeV1Api, NodeV1alpha1Api, NodeV1beta1Api, OpenidApi, PolicyApi, PolicyV1Api, PolicyV1beta1Api, RbacAuthorizationApi, RbacAuthorizationV1Api, RbacAuthorizationV1alpha1Api, SchedulingApi, SchedulingV1Api, SchedulingV1alpha1Api, StorageApi, StorageV1Api, StorageV1alpha1Api, StorageV1beta1Api, VersionApi, WellKnownApi];
