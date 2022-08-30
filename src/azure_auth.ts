6cb6c4f5f36347721f7ca5f9a26f643cce5d26d0
Brendan Burns <bburns@microsoft.com>
2022-08-30 04:00:27 +0000

    Upgrade typescript and package-lock version.

--- src/azure_auth.ts
+++ src/azure_auth.ts
@@ -82,7 +82,7 @@ export class AzureAuth implements Authenticator {
         try {
             output = proc.execSync(cmd);
         } catch (err) {
-            throw new Error('Failed to refresh token: ' + err.message);
+            throw new Error('Failed to refresh token: ' + (err as Error).message);
         }
 
         const resultObj = JSON.parse(output);
