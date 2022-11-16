25d71e676e39f2dbb26c3940e6431a8c4599476e
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2022-11-16 22:15:07 +0200

    add test in GCP as well

--- src/json_path_test.ts
+++ src/json_path_test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { jsonpath } from './json_path';
 
 describe('jsonpath', () => {
-    it('should throw if vulnerable for RCE (remote code execution)', () => {
+    it('should throw if tried to run JavaScript inside the path', () => {
         expect(() => {
             jsonpath('$..[?(' + '(function a(arr){' + 'a([...arr, ...arr])' + '})([1]);)]', {
                 nonEmpty: 'object',
