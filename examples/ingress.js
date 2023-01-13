e848e4d97c07156b73d743e3cc0e256dd5d5115c
vBitza <37511091+vBitza@users.noreply.github.com>
2023-01-13 21:04:01 +0200

    update ingress example to NetworkingV1Api apiVersion

--- examples/ingress.js
+++ examples/ingress.js
@@ -1,27 +1,43 @@
-const k8s = require('@kubernetes/client-node')
-const kc = new k8s.KubeConfig()
-kc.loadFromDefault()
+const k8s = require('@kubernetes/client-node');
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.NetworkingV1beta1Api) // before 1.14 use extensions/v1beta1
-const clientIdentifier = 'my-subdomain'
+const k8sApi = kc.makeApiClient(k8s.NetworkingV1Api); // before 1.14 use extensions/v1beta1
+const clientIdentifier = 'my-subdomain';
 
 k8sApi.createNamespacedIngress('default', {
-  apiVersions: 'networking.k8s.io/v1beta1',
+  apiVersions: 'networking.k8s.io/v1',
   kind: 'Ingress',
-  metadata: { name: `production-custom-${clientIdentifier}` },
+  metadata: {
+    name: `production-custom-${clientIdentifier}`,
+    labels: {
+      createdBy: 'node-client',
+    },
+    annotations: {
+      'meta.helm.sh/release-namespace': 'production-auto-deploy',
+    },
+  },
   spec: {
+    ingressClassName: 'nginx',
     rules: [{
-      host: `${clientIdentifier}.example.com`,
+      host: `${clientIdentifier}`,
       http: {
         paths: [{
           backend: {
-            serviceName: 'production-auto-deploy',
-            servicePort: 5000
+            service: {
+              name: 'production-auto-deploy',
+              port: {
+                number: 5000,
+              },
+            },
           },
-          path: '/'
-        }]
-      }
+          path: '/default-kuberiq(/|$)(.*)',
+          pathType: 'ImplementationSpecific',
+        }],
+      },
     }],
-    tls: [{ hosts: [`${clientIdentifier}.example.com`] }]
-  }
-}).catch(e => console.log(e))
+    tls: [{
+      hosts: [`${clientIdentifier}.example.com`],
+    }],
+  },
+}).catch(e => console.log(e));
