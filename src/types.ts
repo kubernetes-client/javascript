5dc061f0aa37baf542a92b3dd2bec82aead87cc2
Ben Cooper <benjamin.cooper@convoy.com>
2023-08-24 11:50:38 -0700

    Add optional spec property to Kubernetes object

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,11 @@
 import { V1ListMeta, V1ObjectMeta } from './api';
 
+
 export interface KubernetesObject {
     apiVersion?: string;
     kind?: string;
     metadata?: V1ObjectMeta;
+    spec?: object;
 }
 
 export interface KubernetesListObject<T extends KubernetesObject> {
