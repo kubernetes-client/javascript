0571766c71e0a0a6c350f42bbb761bbf41fc5fb4
Christopher Dieringer <cdaringe@users.noreply.github.com>
2023-10-05 14:04:15 -0700

    docs: exec example

--- examples/typescript/exec/exec-example.ts
+++ examples/typescript/exec/exec-example.ts
@@ -1,8 +1,6 @@
 import * as k8s from '@kubernetes/client-node';
 import * as stream from 'stream';
 
-const command = process.argv[2];
-
 const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 
@@ -11,7 +9,7 @@ exec.exec(
     'default',
     'nginx-4217019353-9gl4s',
     'nginx',
-    command,
+    ["ls, "-al", "."],
     process.stdout as stream.Writable,
     process.stderr as stream.Writable,
     process.stdin as stream.Readable,
