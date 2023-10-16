f3af15b5068181bfc392511256aa4fc78620c73c
David Gamero <david340804@gmail.com>
2023-10-16 14:15:31 -0400

    audit fix

--- examples/typescript/patch/patch-example.ts
+++ examples/typescript/patch/patch-example.ts
@@ -1,6 +1,13 @@
-import cluster from "cluster";
-import { CoreV1Api, Middleware, RequestContext, ResponseContext,KubeConfig, createConfiguration, AuthMethodsConfiguration, Configuration, ServerConfiguration } from "../../../dist";
-import { PromiseMiddlewareWrapper } from "../../../dist/gen/middleware";
+import {
+    CoreV1Api,
+    RequestContext,
+    ResponseContext,
+    KubeConfig,
+    createConfiguration,
+    Configuration,
+    ServerConfiguration,
+} from '../../../dist';
+import { PromiseMiddlewareWrapper } from '../../../dist/gen/middleware';
 
 // const k8s = require('@kubernetes/client-node');
 
@@ -9,57 +16,67 @@ kc.loadFromDefault();
 
 const k8sApi = kc.makeApiClient(CoreV1Api);
 
-k8sApi.listNamespacedPod({
-    namespace: 'default'
-})
-.then((res) => {
-    const patch = [
-        {
-            "op": "replace",
-            "path":"/metadata/labels",
-            "value": {
-                "foo": "bar"
-            }
-        }
-    ];
-    const options = { "headers": { "Content-type": "application/json-patch+json"}};
-    const headerPatchMiddleware = new PromiseMiddlewareWrapper({
-        pre: async (requestContext: RequestContext) => {
-            requestContext.setHeaderParam("Content-type",  "application/json-patch+json");
-            return requestContext;
-        },
-        post: async (responseContext: ResponseContext) => responseContext
-    })
-    let currentContext = kc.getCurrentContext();
-    let currentCluster = kc.getCluster(currentContext);
-    if (currentCluster === undefined || currentCluster === null) {
-        throw new Error("Cluster is undefined");
-    }
-    let server = currentCluster.server ;
-    if (server === undefined) {
-        throw new Error("Server is undefined");
-    }
-
-    const baseServerConfig: ServerConfiguration<{}> = new ServerConfiguration<{}>(server, {});
-    const configuration: Configuration = createConfiguration({
-        middleware: [headerPatchMiddleware],
-        baseServer: baseServerConfig,
-        authMethods: {
-            default: kc,
-        },
-    });
-
-    let podName = res.items[0].metadata?.name;
-    if (podName === undefined) {
-        throw new Error("Pod name is undefined");
-    }
-    k8sApi.patchNamespacedPod({
-        name: podName,
+k8sApi
+    .listNamespacedPod({
         namespace: 'default',
-        body: patch,
-    },
-    configuration
-    )
-    .then(() => { console.log("Patched.")})
-    .catch((err) => { console.log("Error: "); console.log(err)});
-});
+    })
+    .then((res) => {
+        const patch = [
+            {
+                op: 'replace',
+                path: '/metadata/labels',
+                value: {
+                    foo: 'bar',
+                },
+            },
+        ];
+        const options = { headers: { 'Content-type': 'application/json-patch+json' } };
+        const headerPatchMiddleware = new PromiseMiddlewareWrapper({
+            pre: async (requestContext: RequestContext) => {
+                requestContext.setHeaderParam('Content-type', 'application/json-patch+json');
+                return requestContext;
+            },
+            post: async (responseContext: ResponseContext) => responseContext,
+        });
+        let currentContext = kc.getCurrentContext();
+        let currentCluster = kc.getCluster(currentContext);
+        if (currentCluster === undefined || currentCluster === null) {
+            throw new Error('Cluster is undefined');
+        }
+        let server = currentCluster.server;
+        if (server === undefined) {
+            throw new Error('Server is undefined');
+        }
+
+        const baseServerConfig: ServerConfiguration<{}> = new ServerConfiguration<{}>(server, {});
+        const configuration: Configuration = createConfiguration({
+            middleware: [headerPatchMiddleware],
+            baseServer: baseServerConfig,
+            authMethods: {
+                default: kc,
+            },
+        });
+
+        let podName = res.items[0].metadata?.name;
+        if (podName === undefined) {
+            throw new Error('Pod name is undefined');
+        }
+        k8sApi
+            .patchNamespacedPod(
+                {
+                    name: podName,
+                    namespace: 'default',
+                    body: patch,
+                },
+                configuration,
+            )
+            .then(() => {
+                // tslint:disable-next-line:no-console
+                console.log('Patched.');
+            })
+            .catch((err) => {
+                // tslint:disable-next-line:no-console
+                console.log('Error: ');
+                console.log(err);
+            });
+    });
