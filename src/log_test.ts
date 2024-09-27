7bc01b60e8d688162178b3374a7df636caa96c2b
cjihrig <cjihrig@gmail.com>
2024-09-27 13:00:40 -0400

    logs: add support for sinceTime option
    This commit adds sinceTime support to LogOptions.
    
    Fixes: https://github.com/kubernetes-client/javascript/issues/1673

--- src/log_test.ts
+++ src/log_test.ts
@@ -5,8 +5,8 @@ import { URLSearchParams } from 'url';
 describe('Log', () => {
     describe('AddOptionsToSearchParams', () => {
         it('should add options to search params', () => {
-            const searchParams = new URLSearchParams();
-            const options: LogOptions = {
+            let searchParams = new URLSearchParams();
+            let options: LogOptions = {
                 follow: true,
                 limitBytes: 100,
                 pretty: true,
@@ -23,6 +23,12 @@ describe('Log', () => {
             expect(searchParams.get('sinceSeconds')).to.equal('1');
             expect(searchParams.get('tailLines')).to.equal('1');
             expect(searchParams.get('timestamps')).to.equal('true');
+
+            const sinceTime = new Date().toISOString();
+            searchParams = new URLSearchParams();
+            options = { sinceTime };
+            AddOptionsToSearchParams(options, searchParams);
+            expect(searchParams.get('sinceTime')).to.equal(sinceTime);
         });
         it('should use defaults for', () => {
             const searchParams = new URLSearchParams();
@@ -32,6 +38,17 @@ describe('Log', () => {
             expect(searchParams.get('pretty')).to.equal('false');
             expect(searchParams.get('previous')).to.equal('false');
             expect(searchParams.get('timestamps')).to.equal('false');
+        });
+        it('sinceTime and sinceSeconds cannot be used together', () => {
+            const searchParams = new URLSearchParams();
+            const sinceTime = new Date().toISOString();
+            const options: LogOptions = {
+                sinceSeconds: 1,
+                sinceTime,
+            };
+            expect(() => {
+                AddOptionsToSearchParams(options, searchParams);
+            }).to.throw('at most one of sinceTime or sinceSeconds may be specified');
         });
     });
 });
