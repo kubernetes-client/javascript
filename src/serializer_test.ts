3193f6e0cbe4627a718b29bf9dd29d6d3748e002
Thieu Quan Ngoc <ngocthieu245@gmail.com>
2024-12-24 22:29:59 +0700

    fix: KubernetesObjectSerializer::deserialize() should support list api

--- src/serializer_test.ts
+++ src/serializer_test.ts
@@ -186,5 +186,66 @@ describe('KubernetesObjectSerializer', () => {
             const res = KubernetesObjectSerializer.serialize(s, 'unknown');
             expect(res).to.deep.equal(s);
         });
+
+        it('should deserialize a list of unknown objects', () => {
+            const s = {
+                apiVersion: 'v1alpha1',
+                kind: 'MyCustomResourceList',
+                metadata: {
+                    resourceVersion: '1',
+                },
+                items: [
+                    {
+                        apiVersion: 'v1alpha1',
+                        kind: 'MyCustomResource',
+                        metadata: {
+                            name: 'k8s-js-client-test',
+                            namespace: 'default',
+                            creationTimestamp: '2022-01-01T00:00:00.000Z',
+                        },
+                        data: {
+                            key: 'value',
+                        },
+                    },
+                ],
+            };
+            const res = KubernetesObjectSerializer.deserialize(s, 'v1alpha1MyCustomResource');
+            expect(res).to.deep.equal({
+                apiVersion: 'v1alpha1',
+                kind: 'MyCustomResourceList',
+                metadata: {
+                    _continue: undefined,
+                    remainingItemCount: undefined,
+                    resourceVersion: '1',
+                    selfLink: undefined,
+                },
+                items: [
+                    {
+                        apiVersion: 'v1alpha1',
+                        kind: 'MyCustomResource',
+                        metadata: {
+                            name: 'k8s-js-client-test',
+                            namespace: 'default',
+                            creationTimestamp: new Date('2022-01-01T00:00:00.000Z'),
+                            uid: undefined,
+                            annotations: undefined,
+                            labels: undefined,
+                            finalizers: undefined,
+                            generateName: undefined,
+                            selfLink: undefined,
+                            resourceVersion: undefined,
+                            generation: undefined,
+                            ownerReferences: undefined,
+                            deletionTimestamp: undefined,
+                            deletionGracePeriodSeconds: undefined,
+                            managedFields: undefined,
+                        },
+                        data: {
+                            key: 'value',
+                        },
+                    },
+                ],
+            });
+        });
     });
 });
