fe8d1c14c3f1d5c4f326156c4c9864356b381cdf
Ross Anthony <ross@velo33.com>
2025-04-10 11:52:15 -0400

    Reintroduce timeout and keep-alive for watch requests to match client-go

--- src/watch.ts
+++ src/watch.ts
@@ -41,6 +41,7 @@ export class Watch {
         const controller = new AbortController();
         requestInit.signal = controller.signal as AbortSignal;
         requestInit.method = 'GET';
+        requestInit.timeout = 30000;
 
         let doneCalled: boolean = false;
         const doneCallOnce = (err: any) => {
@@ -53,6 +54,12 @@ export class Watch {
 
         try {
             const response = await fetch(watchURL, requestInit);
+
+            if (requestInit.agent && typeof requestInit.agent === 'object') {
+                for (const socket of Object.values(requestInit.agent.sockets).flat()) {
+                    socket?.setKeepAlive(true, 30000);
+                }
+            }
 
             if (response.status === 200) {
                 const body = response.body!;
